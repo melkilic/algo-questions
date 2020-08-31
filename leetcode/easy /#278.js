1c315495f34431d5ed009d83adc70bc07d8f2c2d
melkilic <melike180400@gmail.com>
2020-08-30 21:57:53 -0500

    binary search exercise added

--- leetcode/easy /#278.js
+++ leetcode/easy /#278.js
@@ -1,5 +1,6 @@
 /**
  * /* First Bad Version
+ * Binary Search
  * You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
  *
  * Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
@@ -25,13 +26,12 @@ var solution = function (isBadVersion) {
     let last = n;
 
     while (first < last) {
-      const middle = first + Math.floor((last - first) / 2);
+      let middle = first + Math.floor((last - first) / 2);
 
-      const isMidBad = isBadVersion(middle);
-      if (isMidBad) {
-        last = middle;
-      } else {
+      if (!isBadVersion(middle)) {
         first = middle + 1;
+      } else {
+        last = middle;
       }
     }
 
